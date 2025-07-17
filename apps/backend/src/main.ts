9cdee3e80a3a5d331b6d449431ddd839e90524c3
MwambiaBrian <brianmwambia4@gmail.com>
2025-07-17 16:28:15 +0300

    depl: fix cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // frontend URL
+    origin: [
+      'http://localhost:5173',
+      'https://softwizpro-website-backend.onrender.com',
+    ], // frontend URL
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });
